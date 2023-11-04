6b3b943b35250fbca1ac0a62517d9a8c280387d0
Scott Huskins <scott_husins@yahoo.com>
2023-11-03 23:38:05 -0400

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by MacBookM2 on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
